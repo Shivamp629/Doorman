2c7af1ea2fb88cb57a7e8a814318fdbd4d2c4f2b
Shivam Patel <shivampatel@Shivams-MacBook-Pro.local>
2018-09-08 13:47:47 -0400

    Flask API for users is setup - need to design use cases

--- api/server.py
+++ api/server.py
@@ -1,36 +1,62 @@
-from flask import request, url_for
-from flask_api import FlaskAPI, status, exceptions
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+USERS = {
+    'user1': {'task': 'build an API'},
+    'user2': {'task': '?????'},
+    'user3': {'task': 'profit!'},
+}
 
 
-app = FlaskAPI(__name__)
+def abort_if_user_doesnt_exist(user_id):
+    if user_id not in USERS:
+        abort(404, message="user {} doesn't exist".format(user_id))
+
+parser = reqparse.RequestParser()
+parser.add_argument('task')
 
 
-@app.route("/users", methods=['GET', 'POST'])
-def user_list():
-    if request.method == 'GET':
-        data = str(request.data.get('text', ''))
-        return '', status.HTTP_200_OK
+# user
+# shows a single user item and lets you delete a user item
+class User(Resource):
+    def get(self, user_id):
+        abort_if_user_doesnt_exist(user_id)
+        return USERS[user_id]
 
-    if request.method == 'POST':
-        data = str(request.data.get('text', ''))
-        return '', status.HTTP_201_CREATED
+    def delete(self, user_id):
+        abort_if_user_doesnt_exist(user_id)
+        del USERS[user_id]
+        return '', 204
 
-
-@app.route("/logs/<string:user>/", methods=['GET', 'PUT', 'DELETE'])
-def notes_detail(user):
-    """
-    Retrieve, update or delete note instances.
-    """
-    if request.method == 'PUT':
-        data = str(request.data.get('text', ''))
-        return ''
-
-    elif request.method == 'DELETE':
-        return '', status.HTTP_204_NO_CONTENT
-
-    elif request.method == 'GET':
-        return '', status.HTTP_200_OK
+    def put(self, user_id):
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        USERS[user_id] = task
+        return task, 201
 
 
-if __name__ == "__main__":
+# userList
+# shows a list of all USERS, and lets you POST to add new tasks
+class Users(Resource):
+    def get(self):
+        return USERS
+
+    def post(self):
+        args = parser.parse_args()
+        user_id = int(max(USERS.keys()).lstrip('user')) + 1
+        user_id = 'user%i' % user_id
+        USERS[user_id] = {'task': args['task']}
+        return USERS[user_id], 201
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(Users, '/users')
+api.add_resource(User, '/users/<user_id>')
+
+
+if __name__ == '__main__':
     app.run(debug=True)
