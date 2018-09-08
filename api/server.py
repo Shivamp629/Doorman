9dece2e055eddc674acbd244bebac3d3037f6282
Shivam Patel <shivampatel@Shivams-MacBook-Pro.local>
2018-09-08 15:15:58 -0400

    Setting up spotify endpoints for API requests

--- api/server.py
+++ api/server.py
@@ -1,42 +1,58 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
+import random
 
 app = Flask(__name__)
 api = Api(app)
 
 USERS = {
-    '1': {'user': 'raghav'
-              'playlist':
-                {
+    '1': {'user': 'raghav',
+          'emotion':
+          'playlists':
+            {
+                'happy': []
 
-                }
-              'emotion':
-              },
+                'sad': []
+
+                'angry': []
+            }
+         },
 
     '2': {'user': 'varun'
-              'playlist':
-                {
+          'emotion':
+          'playlists':
+            {
+                'happy': []
 
-                }
-              'emotion':
-              },
+                'sad': []
 
+                'angry': []
+            }
+         },
 
     '3': {'user': 'shivam'
-              'playlist':
-                {
+          'emotion':
+          'playlists':
+            {
+                'happy': []
 
-                }
-              'emotion':
-              },
+                'sad': []
+
+                'angry': []
+            }
+         },
 
     '4': {'user': 'akhila'
-              'playlist':
-                {
+          'emotion':
+          'playlists':
+            {
+                'happy': []
 
-                }
-              'emotion':
-              }
+                'sad': []
+
+                'angry': []
+            }
+         }
 }
 
 
@@ -44,17 +60,11 @@ def abort_if_user_doesnt_exist(user_id):
     if user_id not in USERS:
         abort(404, message="user {} doesn't exist".format(USERS[user_id].user))
 
-parser = reqparse.RequestParser()
-parser.add_argument('task')
-
-
-# user
-# shows a single user item and lets you delete a user item
 class User(Resource):
     def get(self, user_id):
         abort_if_user_doesnt_exist(user_id)
-        return USERS[user_id]
-
+        return USERS.get(user_id)
+    '''
     def delete(self, user_id):
         abort_if_user_doesnt_exist(user_id)
         del USERS[user_id]
@@ -65,28 +75,39 @@ class User(Resource):
         task = {'task': args['task']}
         USERS[user_id] = task
         return task, 201
+    '''
 
-
-# userList
-# shows a list of all USERS, and lets you POST to add new tasks
 class Users(Resource):
     def get(self):
         return USERS
-# userList
-# shows a list of all USERS, and lets you POST to add new tasks
+
 class DetermineUser(Resource):
     def get(self):
         user_id = get_user_id()
-        return {'user': user_id}
+        return {'user_id': user_id}
+
+class Playlist(Resource):
+    def get(self,user_id):
+        user = USERS.get('user_id')
+        emotion = user.get('emotion')
+        playlist = user.get('playlists').get(emotion)
+        return {'playlist': playlist}
+
+class Song(Resource):
+    def get(self,user_id):
+        user = USERS.get('user_id')
+        emotion = user.get('emotion')
+        playlist = user.get('playlists').get(emotion)
+        return {'song': random.choice(playlist)}
 
 ##
 ## Actually setup the Api resource routing here
 ##
 api.add_resource(Users, '/users')
-api.add_resource(User, '/users/<user_id>')
+api.add_resource(User, '/users/<int:user_id>')
 api.add_resource(DetermineUser, '/DetermineUser')
-api.add_resource(Playlist, '/users/<user_id>/playlist')
-api.add_resource(Song, '/users/<user_id>/playlist/song')
+api.add_resource(Playlist, '/users/<int:user_id>/playlist')
+api.add_resource(Song, '/users/<int:user_id>/playlist/song')
 
 if __name__ == '__main__':
     app.run(debug=True)
